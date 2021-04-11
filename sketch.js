a2e016a741e3f5727a09c34ed80a321fcd202c31
DINNU257 <77061721+DINNU257@users.noreply.github.com>
2021-04-11 19:07:28 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -50,7 +50,7 @@ rope4.display();
 }
 function keyPressed(){
 	if (keyCode === UP_ARROW){
-		Matter.Body.applyForce(bob1.Body,bob1.Body.position,{x:- -50,y:- 45})
+		Matter.Body.applyForce(bob1.body,bob1.body.position,{x:- -50,y:- 45})
 	}
 }
 
